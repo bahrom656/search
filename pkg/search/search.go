f1bad6c074e4d2762ababf243fba7e29bbba4cf6
bahrullo <bahrom656>
2020-10-24 15:42:54 +0500

    20.1

---
+++ pkg/search/search.go
@@ -0,0 +1,104 @@
+package search
+
+import (
+	"context"
+	"io"
+	"log"
+	"os"
+	"strings"
+	"sync"
+	"time"
+)
+
+type Result struct {
+	Phrase  string
+	Line    string
+	LineNum int64
+	ColNum  int64
+}
+
+func All(ctx context.Context, phrase string, files []string) <-chan []Result {
+	ch := make(chan []Result)
+	reads := make([]*os.File, 0)
+
+	root := context.Background()
+	ctx, _ = context.WithTimeout(root, time.Second*10)
+
+	for _, file := range files {
+		read, err := os.Open(file)
+
+		if err != nil {
+			log.Print(err)
+		}
+
+		reads = append(reads, read)
+	}
+	wg := sync.WaitGroup{}
+	goroutines := len(files)
+
+	for i := 0; i < goroutines; i++ {
+		wg.Add(1)
+		i := i
+		go func(ch chan<- []Result, ctx context.Context) {
+			defer wg.Done()
+			<-ctx.Done()
+
+			buf := make([]byte, 4094)
+			content := make([]byte, 0)
+
+			results := make([]Result, 0)
+
+			for {
+				read, err := reads[i].Read(buf)
+
+				if err == io.EOF {
+					content = append(content, buf[:read]...)
+					break
+				}
+				if err != nil {
+					log.Print(err)
+				}
+
+				content = append(content, buf[:read]...)
+			}
+			data := string(content)
+
+			datas := strings.Split(data, "\n")
+
+			for line, str := range datas {
+				pos := strings.Index(str, phrase)
+				result := Result{}
+				if pos != -1 {
+					result = Result{
+						Phrase:  phrase,
+						Line:    str,
+						LineNum: int64(line),
+						ColNum:  int64(pos),
+					}
+					results = append(results, result)
+				} else {
+					result = Result{
+						Phrase:  phrase,
+						ColNum:  nil,
+						LineNum: nil,
+						Line:    nil}
+					results = append(results, result)
+				}
+				ch <- results
+			}
+			return
+		}(ch, ctx)
+
+	}
+
+	go func() {
+		defer close(ch)
+		wg.Wait()
+	}()
+
+	<-ctx.Done()
+
+	<-time.After(time.Second)
+
+	return ch
+}
