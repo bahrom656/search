1019a22b4e6e4aca26b421bf83e4c74164975e45
bahrullo <bahrom656>
2020-10-25 04:12:42 +0500

    20.1

--- pkg/search/search.go
+++ pkg/search/search.go
@@ -2,10 +2,10 @@ package search
 
 import (
 	"context"
-	"io"
+	"io/ioutil"
 	"log"
-	"os"
 	"strings"
+	"sync"
 )
 
 type Result struct {
@@ -18,58 +18,59 @@ type Result struct {
 func All(ctx context.Context, phrase string, files []string) <-chan []Result {
 
 	ch := make(chan []Result)
-	reads := make([]*os.File, 0)
-
-	var read *os.File
-	for _, file := range files {
-		read, err := os.Open(file)
-		if err != nil {
-			log.Print(err)
-		}
-
-		reads = append(reads, read)
-
-	}
+	//reads := make([]*os.File, 0)
+	//
+	wg := sync.WaitGroup{}
+	//for _, file := range files {
+	//	read, err := os.Open(file)
+	//	if err != nil {
+	//		log.Print(err)
+	//	}
+	//
+	//	reads = append(reads, read)
+	//
+	//}
 
 	goroutines := len(files)
 
 	for i := 0; i < goroutines; i++ {
-		i := i
-		go func(ch chan<- []Result, ctx context.Context) {
-			buf := make([]byte, 4094)
-			content := make([]byte, 0)
-
-			results := make([]Result, 0)
-
-			for {
-				rd, err := reads[i].Read(buf)
-
-				if err == io.EOF {
-					content = append(content, buf[:rd]...)
-					break
-				}
-				if err != nil {
-					log.Print(err)
-				}
-
-				content = append(content, buf[:rd]...)
+		wg.Add(1)
+		go func(ch chan<- []Result, name string, i int, ctx context.Context) {
+			defer wg.Done()
+			//buf := make([]byte, 4094)
+			//content := make([]byte, 0)
+			//
+			var results []Result
+			//
+			//for {
+			//	rd, err := reads[i].Read(buf)
+			//
+			//	if err == io.EOF {
+			//		content = append(content, buf[:rd]...)
+			//		break
+			//	}
+			//	if err != nil {
+			//		log.Print(err)
+			//	}
+			//
+			//	content = append(content, buf[:rd]...)
+			//}
+			data, err := ioutil.ReadFile(name)
+			if err != nil {
+				log.Println(err)
 			}
-			data := string(content)
 
-			datas := strings.Split(data, string(rune(13))+string(rune(10)))
+			datas := strings.Split(string(data), "\n")
 			for line, str := range datas {
 				pos := strings.Index(str, phrase)
 				if pos != -1 {
-					result := Result{}
-					result = Result{
+					result := Result{
 						Phrase:  phrase,
 						Line:    str,
 						LineNum: int64(line + 1),
 						ColNum:  int64(pos + 1),
 					}
-					if	result.ColNum != -1{
-						results = append(results, result)
-					}
+					results = append(results, result)
 				} //} else {
 			}
 			//	result = Result{
@@ -79,14 +80,15 @@ func All(ctx context.Context, phrase string, files []string) <-chan []Result {
 			//		Line:    ""}
 			//	results = append(results, result)
 			//}
-			ch <- results
-
-		}(ch, ctx)
-
+			if len(results) > 0 {
+				ch <- results
+			}
+		}(ch, files[i], i, ctx)
 	}
 	go func() {
-		defer read.Close()
-		return
+		defer close(ch)
+		wg.Wait()
 	}()
+
 	return ch
 }
