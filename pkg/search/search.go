53d376ebf78852aac5febb11b2a60f19be67c1e0
bahrullo <bahrom656>
2020-10-26 21:09:14 +0500

    20.2

--- pkg/search/search.go
+++ pkg/search/search.go
@@ -92,3 +92,78 @@ func All(ctx context.Context, phrase string, files []string) <-chan []Result {
 
 	return ch
 }
+
+func Any(ctx context.Context, phrase string, files []string) <-chan Result {
+
+	ch := make(chan Result)
+	//reads := make([]*os.File, 0)
+	//
+	wg := sync.WaitGroup{}
+	//for _, file := range files {
+	//	read, err := os.Open(file)
+	//	if err != nil {
+	//		log.Print(err)
+	//	}
+	//
+	//	reads = append(reads, read)
+	//
+	//}
+
+	goroutines := len(files)
+
+	for i := 0; i < goroutines; i++ {
+		wg.Add(1)
+		go func(ch chan<- Result, name string, i int, ctx context.Context) {
+			defer wg.Done()
+			//buf := make([]byte, 4094)
+			//content := make([]byte, 0)
+			//
+			//
+			//for {
+			//	rd, err := reads[i].Read(buf)
+			//
+			//	if err == io.EOF {
+			//		content = append(content, buf[:rd]...)
+			//		break
+			//	}
+			//	if err != nil {
+			//		log.Print(err)
+			//	}
+			//
+			//	content = append(content, buf[:rd]...)
+			//}
+			var result Result
+			data, err := ioutil.ReadFile(name)
+			if err != nil {
+				log.Println(err)
+			}
+
+			datas := strings.Split(string(data), "\n")
+			for line, str := range datas {
+				pos := strings.Index(str, phrase)
+				if pos != -1 {
+					result = Result{
+						Phrase:  phrase,
+						Line:    str,
+						LineNum: int64(line + 1),
+						ColNum:  int64(pos + 1),
+					}
+				} //} else {
+			}
+			//	result = Result{
+			//		Phrase:  phrase,
+			//		ColNum:  0,
+			//		LineNum: 0,
+			//		Line:    ""}
+			//	results = append(results, result)
+			//}
+			ch <- result
+		}(ch, files[i], i, ctx)
+	}
+	go func() {
+		defer close(ch)
+		wg.Wait()
+	}()
+
+	return ch
+}
---
+++ pkg/search/search_test.go
@@ -0,0 +1,15 @@
+package search
+
+import "testing"
+
+func TestAll(t *testing.T) {
+	var files []string
+	file := "../data/1.txt"
+	files = append(files, file)
+	file = "../data/2.txt"
+	files = append(files, file)
+	result := All(nil,"root", files)
+	results := <- result
+	t.Error(results)
+
+}
