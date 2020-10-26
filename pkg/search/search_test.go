0a99c01851d02bdd52c53a6d290de82b9c20eb43
bahrullo <bahrom656>
2020-10-26 21:24:27 +0500

    20.2

--- pkg/search/search_test.go
+++ pkg/search/search_test.go
@@ -1,15 +1,13 @@
 package search
 
-import "testing"
-
-func TestAll(t *testing.T) {
-	var files []string
-	file := "../data/1.txt"
-	files = append(files, file)
-	file = "../data/2.txt"
-	files = append(files, file)
-	result := All(nil,"root", files)
-	results := <- result
-	t.Error(results)
-
-}
+//func TestAll(t *testing.T) {
+//	var files []string
+//	file := "./data/1.txt"
+//	files = append(files, file)
+//	file = "./data/2.txt"
+//	files = append(files, file)
+//	result := All(nil,"root", files)
+//	results := <- result
+//	t.Error(results)
+//
+//}
